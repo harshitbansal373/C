7c712bf5d7bb45e9de0ca39741748237e0f1abff
harshitbansal373 <harshitbansal373@gmail.com>
2018-10-12 15:13:26 +0530

    add area of circle

---
+++ AreaOfCircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+   int x;
+   float area;
+   printf("plz enter value for x");
+   scanf("%d",&x);
+   area=3.14*x*x;
+   printf("%f",area);
+   return 0;
+}
