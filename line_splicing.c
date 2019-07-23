2ea944749b3b66a9b5c1b03aee11f42fa3905861
harshit bansal <harshitbansal373@gmail.com>
2019-07-24 00:09:45 +0530

    line splicing

---
+++ line_splicing.c
@@ -0,0 +1,11 @@
+// C program to illustrate the concept of Line splicing. 
+#include <stdio.h> 
+int main() 
+{ 
+    // Line Splicing\ 
+    printf("Hello GFG\n"); 
+    printf("welcome"); 
+    return (0); 
+} 
+//Output:
+//welcome
