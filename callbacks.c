1876590f166f482d0946b8d713394d1d3397d08b
harshit bansal <harshitbansal373@gmail.com>
2019-07-24 10:59:33 +0530

    callbacks in c

---
+++ callbacks.c
@@ -0,0 +1,27 @@
+// A simple C program to demonstrate callback 
+#include<stdio.h> 
+  
+void A() 
+{ 
+    printf("I am function A\n"); 
+} 
+  
+// callback function 
+void B(void (*ptr)()) 
+{ 
+    (*ptr) (); // callback to A 
+} 
+  
+int main() 
+{ 
+    void (*ptr)() = &A; 
+      
+    // calling function B and passing 
+    // address of the function A as argument 
+    B(ptr); 
+  
+   return 0; 
+}
+
+//output:
+//I am function A
