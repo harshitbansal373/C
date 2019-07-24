f939577c887bd8f3852033cb6da9e6b59714ec08
harshit bansal <harshitbansal373@gmail.com>
2019-07-24 14:47:08 +0530

    power function

---
+++ pow.c
@@ -0,0 +1,18 @@
+// power function 
+#include<bits/stdc++.h> 
+  
+using namespace std; 
+  
+int main() 
+{ 
+    double x = 6.1, y = 4.8; 
+  
+    // Storing the answer in result. 
+    double result = pow(x, y); 
+    cout << result << endl; 
+  
+    return 0; 
+} 
+
+//Output:
+//5882.79
