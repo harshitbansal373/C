9a9ae1cbbfa7bf1f609c7d00da25d68d7c997fbf
harshitbansal373 <harshitbansal373@gmail.com>
2019-10-06 10:36:56 +0530

    insertion sorting

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -1,27 +1,32 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
-{
-clrscr();
-int a[20],size,i,j,temp;
-printf("enter size of array");
-scanf("%d",&size);
-printf("enter elements of array");
-for(i=0;i<size;i++)
-scanf("%d",&a[i]);
-//insertion sorting
-for(i=1;i<size;i++)
-{
-	temp=a[i];
-	for(j=i-1;j>=0 && a[j]>temp;j--)
-	{
-		a[j+1]=a[j];
-		a[j]=temp;
-	}
-
-}
-printf("array after insertion sort");
-for(i=0;i<size;i++)
-printf("%2d",a[i]);
-getch();
+int main(){
+    int i,n,ptr,temp,value;
+    printf("enter number of number elements in array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nenter array elements");
+    for(i=0;i<n;i++){
+        scanf("%d",&value);
+        arr[i]=value;
+    }
+    printf("\narray elements\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    
+    for(i=1;i<n;i++){
+        temp=arr[i];
+        ptr=i-1;
+        while(ptr>=0 && temp<arr[ptr]){
+            arr[ptr+1]=arr[ptr];
+            ptr=ptr-1;
+        }
+        arr[ptr+1]=temp;
+    } 
+    
+    printf("\ninsertion sorted array\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
 }
