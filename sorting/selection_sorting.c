fe67374edceb352cb85bf3a75d2b643a6b937511
harshitbansal373 <harshitbansal373@gmail.com>
2019-10-06 10:52:39 +0530

    selection sort

--- sorting/selection_sorting.c
+++ sorting/selection_sorting.c
@@ -1,34 +1,36 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
-{
-clrscr();
-int a[20],i,j,size,temp,loc,min;
-printf("enter array size");
-scanf("%d",&size);
-printf("enter array element");
-for(i=0;i<size;i++)
-scanf("%d",&a[i]);
-//selection sorting
-for(i=0;i<(size-1);i++)
-{
-	min=a[i];
-	loc=i;
-
-	for(j=i+1;j<size;j++)
-	{
-		if(a[j]>min)
-		{
-			min=a[j];
-			loc=j;
-		}
-	}
-	 temp=a[i];
-	 a[i]=a[loc];
-	 a[loc]=temp;
-}
-printf("array of the selection sort");
-for(i=0;i<size;i++)
-printf("%2d",a[i]);
-getch();
+int main(){
+    int i,n,j,min,loc,temp,value;
+    printf("enter number of number elements in array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nenter array elements");
+    for(i=0;i<n;i++){
+        scanf("%d",&value);
+        arr[i]=value;
+    }
+    printf("\narray elements\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    
+    for(i=0;i<n;i++){
+        min=arr[i];
+        loc=i;
+        for(j=i+1;j<n;j++){
+            if(min>arr[j]){
+                min=arr[j];
+                loc=j;
+            }
+        }
+        temp=arr[i];
+        arr[i]=arr[loc];
+        arr[loc]=temp;
+    }
+    
+    printf("\nselection sorted array\n");
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
 }
